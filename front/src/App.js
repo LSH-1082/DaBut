97f3330b53d33c75866e8ddad14da1a7e6357052
LSH-1082 <tkdgus2282@naver.com>
2024-05-04 15:02:38 +0900

    매칭내역 페이지 완료

--- front/src/App.js
+++ front/src/App.js
@@ -4,6 +4,8 @@ import Login from "./login/Login";
 import Main from "./main/Main";
 import Register from "./register/Register";
 import Profile from "./profile/Profile";
+import Alarm from "./alarm/alarm";
+import MatchingCheck from "./matching/matchingCheck";
 
 
 function App() {
@@ -15,8 +17,8 @@ function App() {
                         <Route path="/" element={<Login/>}></Route>
                         <Route path="/register" element={<Register/>}></Route>
                         <Route path="/main" element={<Main/>}></Route>
-                        <Route path="/list" element={<Profile/>}></Route>
-                        <Route path="/alarm" element={<Profile/>}></Route>
+                        <Route path="/list" element={<MatchingCheck/>}></Route>
+                        <Route path="/alarm" element={<Alarm/>}></Route>
                         <Route path="/profile" element={<Profile/>}></Route>
                         {/* 상단에 위치하는 라우트들의 규칙을 모두 확인, 일치하는 라우트가 없는경우 처리 */}
                     </Routes>
