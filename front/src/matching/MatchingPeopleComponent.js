23c937ecd7b91e4b20de7003c01b6a74e0e4169b
LSH-1082 <tkdgus2282@naver.com>
2024-05-04 15:44:44 +0900

    매칭내역 페이지 수정

--- front/src/matching/MatchingPeopleComponent.js
+++ front/src/matching/MatchingPeopleComponent.js
@@ -5,6 +5,8 @@ const MatchingPeopleComponent = () => {
     const user = useSelector(state => state.user);
 
     //TODO matchingPeopleDiv 백엔드에서 데이터 받아서 for문으로 랜더링 해야함
+    //TODO peopleFace에 들어가는 동물 얼굴 컴포넌트로 만들어서(동물 얼굴 컴포넌트를 회원가입 페이지에도 적용) 백엔드 값 받고 랜더링
+    //TODO peopleFaceText에 들어가는 닉네임도 값 받아서 랜더링
 
     return (
         <div className="MatchingPeople">
