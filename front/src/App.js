737b8b8a2456d024323814fa44f93f22078bdba5
yeonji <86038919+duswl4311@users.noreply.github.com>
2024-05-10 17:31:08 +0900

    MainFooter 페이지 수정

--- front/src/App.js
+++ front/src/App.js
@@ -6,7 +6,6 @@ import Register from "./register/Register";
 import Profile from "./profile/Profile";
 import MatchingCheck from "./matching/matchingCheck";
 import RoommatePage from "./roommate/RoommatePage";
-import AlarmPage from "./alarm/AlarmPage";
 
 
 function App() {
@@ -18,8 +17,7 @@ function App() {
                         <Route path="/" element={<Login/>}></Route>
                         <Route path="/register" element={<Register/>}></Route>
                         <Route path="/main" element={<Main/>}></Route>
-                        <Route path="/matchingCheck" element={<MatchingCheck/>}></Route>
-                        <Route path="/alarm" element={<AlarmPage/>}></Route>
+                        <Route path="/list" element={<MatchingCheck/>}></Route>
                         <Route path="/profile" element={<Profile/>}></Route>
                         <Route path="/roommate" element={<RoommatePage/>}></Route>
                         {/* 상단에 위치하는 라우트들의 규칙을 모두 확인, 일치하는 라우트가 없는경우 처리 */}
